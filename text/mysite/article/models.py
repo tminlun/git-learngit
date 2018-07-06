51a7e18867a36a7f3208aae72c02e38e88da4738
tminlun <1272443075@qq.com>
2018-07-06 19:37:35 +0800

    2018/7/6 19pm

---
+++ text/mysite/article/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)#参数为添加时的时间,改或添加时不会变动
+    last_updated_time = models.DateTimeField(auto_now=True)#参数为你改或添加时间为你现在改的时间
+
+    #标注后台的模型具体是哪个模型的对象
+    def __str__(self):
+        return "<Article: %s>" % self.title
---
+++ text/mysite/article/views.py
@@ -0,0 +1,19 @@
+﻿from django.shortcuts import render,render_to_response,get_object_or_404
+from django.http import HttpResponse,Http404
+from .models import Article
+
+# Create your views here.
+
+def article_detail(request,article_id):
+    article = get_object_or_404(Article,id = article_id)
+    context = {}
+    context['article_obj'] = article
+    return render_to_response("article_detail.html",context)
+
+def article_list(request):
+    articles = Article.objects.all()#文章对象,获取全部文章列表
+    #获取文章页面
+    context = {}
+    context['articles'] = articles
+    return render_to_response("article_list.html",context)
+
